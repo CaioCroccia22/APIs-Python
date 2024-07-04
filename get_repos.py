67fb946d84d756db99a39a58d24fcdde3be496c0
caiocroccia22 <caiocroccia22@gmail.com>
2024-07-04 19:10:56 -0300

    fix: chaging url in base_api

--- get_repos.py
+++ get_repos.py
@@ -2,10 +2,10 @@ import requests
 
 # 1 - Mapeando a informação 
 
-headers = {'X-GitHub-Api-Version': '2022-11-18'}
+headers = {'X-GitHub-Api-Version': '2022-11-28'}
 
 
-base_api = 'https:\\api.github.com'
+base_api = 'https://api.github.com'
 user = 'OneBitCodeBlog'
 url = f'{base_api}/users/{user}/repos'
 
@@ -15,4 +15,5 @@ url = f'{base_api}/users/{user}/repos'
 # 2 - Realizando a requisição
 response = requests.get(url, headers=headers)
 print(response.status_code)
-print(len(response.json()))
+# print(len(response.json()))
+# print(response.json())
