ab5afc7c6b062b44fee5902d138b20815a9099ff
caiocroccia22 <caiocroccia22@gmail.com>
2024-07-03 21:56:21 -0300

    feat: trying to do get from repository git

---
+++ get_repos.py
@@ -0,0 +1,18 @@
+import requests
+
+# 1 - Mapeando a informação 
+
+headers = {'X-GitHub-Api-Version': '2022-11-18'}
+
+
+base_api = 'https:\\api.github.com'
+user = 'OneBitCodeBlog'
+url = f'{base_api}/users/{user}/repos'
+
+# print(url)
+
+
+# 2 - Realizando a requisição
+response = requests.get(url, headers=headers)
+print(response.status_code)
+print(len(response.json()))
