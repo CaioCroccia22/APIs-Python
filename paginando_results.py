4dc7bbb30d574d8065a4a402916d723ea2bafd5c
caiocroccia22 <caiocroccia22@gmail.com>
2024-07-07 23:47:33 -0300

    feat: getting an repos name

--- paginando_results.py
+++ paginando_results.py
@@ -5,4 +5,25 @@ access_token = ''
 headers = {
     'Authorization': 'Bearer ' +access_token,
     'X-GitHub-Api-Version': '2022-11-28'
+    
 }
+# 2 - Mapeando Informações
+base_api = 'https://api.github.com'
+user = 'OneBitCodeBlog'
+url = f'{base_api}/users/{user}/repos?page=2'
+
+
+# 3 - Organizando os Dados
+repos_list = []
+for page_num in range(1, 3):
+    try:
+        url_page = f'{url}?page={page_num}'
+        response = requests.get(url, headers=headers)
+        repos_list.append(response.json())
+    except:
+        repos_list.append(None)
+    
+
+# 4 - Apresentar os dados
+print(len(repos_list))
+print(repos_list[0][2]['name'])
