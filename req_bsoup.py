d676874d4a8de1c45c00b4513705d17e949190bc
caiocroccia22 <caiocroccia22@gmail.com>
2024-07-18 22:10:41 -0300

    feat: trying to find all li with clearfix jobs-bx wht-shd-bx class

--- req_bsoup.py
+++ req_bsoup.py
@@ -5,4 +5,8 @@ import requests
 response = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=')
 print(response.status_code)
 
-print(response.text)
+# print(response.text)
+
+soup = BeautifulSoup(response.text, 'lxml')
+jobs = soup.find_all('li', class_='clearfix jobs-bx wht-shd-bx')
+print(len(jobs))
